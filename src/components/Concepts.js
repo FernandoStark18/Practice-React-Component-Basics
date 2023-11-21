9f4023c23da50ce287d8032f31217d2e89aa5333
Fernando Espericueta <fernandostark18@gmail.com>
2023-11-20 18:59:32 -0600

    Practice completed

---
+++ src/components/Concepts.js
@@ -0,0 +1,24 @@
+import Concept from "./Concept";
+
+const Concepts = (props) => {
+  return (
+    <ul id="concepts">
+      <Concept
+        title={props.concepts[0].title}
+        image={props.concepts[0].image}
+        description={props.concepts[0].description}
+      />
+      <Concept
+        title={props.concepts[1].title}
+        image={props.concepts[1].image}
+        description={props.concepts[1].description}
+      />
+      <Concept
+        title={props.concepts[2].title}
+        image={props.concepts[2].image}
+        description={props.concepts[2].description}
+      />
+    </ul>
+  );
+};
+export default Concepts;
